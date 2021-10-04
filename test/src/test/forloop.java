6f2e8ae6467384b26bda458ac6bdc241d0f43532
randipgaur <randipgaur@randips-air.hub>
2021-10-04 22:12:28 +1300

    third commit

---
+++ test/src/test/forloop.java
@@ -0,0 +1,14 @@
+package test;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++ ) {
+		System.out.println("Hello World");
+		}
+		
+
+	}
+
+}
